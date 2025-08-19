123f0c7a987863b1a2707d6cb4aa879ab7478e10
Matheus Braga <sbmatheus20@gmail.com>
2025-08-18 22:21:44 -0300

    sintaxe em js

--- classroom/cs01/scripts.js
+++ classroom/cs01/scripts.js
@@ -1,2 +1,9 @@
-// O console.log exibe no console um texto.
+// Sintaxe correta.
 console.log("Hello World!")
+
+// Sintaxe incorreta.
+// con.sole.log("Olá, Matheus!")
+
+// No JavaScript ponto e vírgula é opcional.
+console.log("Olá, Matheus!");
+console.log("Olá, Matheus!")
