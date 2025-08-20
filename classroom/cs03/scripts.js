666955171117653199440b4fbdd5903b5386fd5e
Matheus Braga <sbmatheus20@gmail.com>
2025-08-19 22:22:59 -0300

    Case Sensitive

---
+++ classroom/cs03/scripts.js
@@ -0,0 +1,14 @@
+/* 
+
+Quando uma linguagem de programação é case-sensitive significa que ela é sensível a letras maiúsculas e minúsculas.
+
+Por exemplo: "Matheus" é considerado diferente de "matheus".
+
+*/
+
+
+var product = "Teclado Mecânico"
+var Product = "Mouse Gamer"
+
+console.log(product)
+console.log(Product)
