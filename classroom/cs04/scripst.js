47feaca48f8e267725d9113f3070cf94a90d9cb6
Matheus Braga <sbmatheus20@gmail.com>
2025-08-19 22:29:57 -0300

    usando let

---
+++ classroom/cs04/scripst.js
@@ -0,0 +1,10 @@
+// Declara uma variável sem valor.
+let user
+console.log("Matheus Braga")
+
+// Declara uma variável com valor.
+let email = "matheus12@gmail.com"
+console.log(email)
+
+email = "matheus12@gmail.com"
+console.log(email)
