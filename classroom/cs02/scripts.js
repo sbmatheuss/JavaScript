488a2d1a6924591073b7c6b768d16547b476f821
Matheus Braga <sbmatheus20@gmail.com>
2025-08-19 22:11:32 -0300

    declaração de variável

---
+++ classroom/cs02/scripts.js
@@ -0,0 +1,14 @@
+// Declara uma variável sem valor.
+var user
+
+console.log(user)
+
+// Declara uma variável com valor.
+
+var email = "matheus12@gmail.com"
+console.log(email)
+
+// Substitui o valor da variável. 
+
+email = "matheus12@gmail.com"
+console.log(email)
