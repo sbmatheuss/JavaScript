943c7c557e1d5d68d10f71f7a7524370c227a806
Matheus Braga <sbmatheus20@gmail.com>
2025-08-21 19:48:24 -0300

    aprendendo const

---
+++ classroom/cs05/scripts.js
@@ -0,0 +1,8 @@
+// Uma constante cria uma variável cujo valor é fixo (não pode ser alterado)
+
+const number = 42
+console.log(number)
+
+// Irá gerar um erro porque o valor não pode ser alterado.
+number = 55
+console.log(number)
