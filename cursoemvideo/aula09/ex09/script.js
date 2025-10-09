598b793334d6611dac8e0dffc01adfe0a54fcdbb
Matheus Braga <sbmatheus20@gmail.com>
2025-10-09 20:33:21 -0300

    exercicio finalizado

--- cursoemvideo/aula09/ex09/script.js
+++ cursoemvideo/aula09/ex09/script.js
@@ -1,11 +1,38 @@
-function calcular() {
+function contar() {
 
-  var start = document.querySelector('input#inicio')
-  var and = document.querySelector('input#Fm')
-  var passo = document.querySelector('input#Pass')
-  var res = document.querySelector('div#res')
+  let ini = document.getElementById('txti')
+  let fim = document.getElementById('txtf')
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('res')
+  
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossivel contar!'
+    alert('[ERRO] Faltam dados!')
+  } else {
+    res.innerHTML = 'Contando: <br>'
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if(p <= 0) {
+      alert('Passo invalido! Considerando PASSO 1')
+      p = 1
+    }
+    
+    if(i < f){
+      //Contagem crescente
+      for(let c = i; c <= f; c += p){
+        res.innerHTML += ` ${c} \u{1F449}`
+      }
+    } else {
+      // Contagem Decrescente
+      for(let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`
+  }
+
 
   
-
   
 }
