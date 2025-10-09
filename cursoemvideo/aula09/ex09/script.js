93a85fd7b478963412e138b83c9c4ee3518851d8
Matheus Braga <sbmatheus20@gmail.com>
2025-10-08 23:03:34 -0300

    criando script do exercicio

---
+++ cursoemvideo/aula09/ex09/script.js
@@ -0,0 +1,11 @@
+function calcular() {
+
+  var start = document.querySelector('input#inicio')
+  var and = document.querySelector('input#Fm')
+  var passo = document.querySelector('input#Pass')
+  var res = document.querySelector('div#res')
+
+  
+
+  
+}
